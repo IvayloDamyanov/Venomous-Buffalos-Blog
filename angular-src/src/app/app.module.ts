4e99ce5f5b216a4a283b1aa12d0113a1dc0d03e3
Milena Shiskova <shishkova.milena@gmail.com>
2017-08-28 18:40:24 +0300

    Added validation service and flash messages

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'ngx-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -34,7 +35,8 @@ const appRoutes : Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
   providers: [ValidateService],
   bootstrap: [AppComponent]
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
+import { ValidateService } from './../../services/validate.service';
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'ngx-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +12,37 @@ export class RegisterComponent implements OnInit {
   username: String;
   email: String;
   password: String;
+  confirmpassword: String;
 
-  constructor() { }
+  constructor(private validateService : ValidateService, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
 
   }
-
+  
   onRegisterSubmit(){
       const user = {
         name: this.name,
         email: this.email,
         username: this.username,
-        password: this.password
+        password: this.password,
+        confirmpassword: this.confirmpassword
       }
+
+  // Apply the validations
+
+  // Validate Email
+  if(!this.validateService.validateEmail(user.email)) {
+    this.flashMessage.show('Please enter a valid email', {classes: ['alert', 'aler-danger'], timeout: 3000})
+    return false;
+  }
+
+  // Required fields
+    if(!this.validateService.validateRegister(user)) {
+    this.flashMessage.show('Please fill in all fields', {classes: ['alert' ,'aler-danger'], timeout: 3000})
+    return false;
+  }
+
     }
 
 }
