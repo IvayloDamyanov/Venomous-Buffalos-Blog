4aba99af8b347e8b01cfe86b73e1a97ce9c10db4
Milena Shiskova <shishkova.milena@gmail.com>
2017-09-02 21:15:32 +0300

    Fixed routing in app-routing.module

---
+++ angular-src/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path:'register', component: RegisterComponent },
+    { path:'login', component: LoginComponent },
+    { path:'profile', component: ProfileComponent },
+    // {path: '**', component: NotFoundComponent}
+];
+
+// const appRoutes : Routes = [
+//   {path:'', component: HomeComponent},
+//   {path:'register', component: RegisterComponent},
+//   {path:'login', component: LoginComponent},
+//   {path:'profile', component: ProfileComponent},
+//   //{path: '**', component: NotFoundComponent},
+// ]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { AuthService } from './services/auth.service';
 import { ValidateService } from './services/validate.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,40 +10,39 @@ import { FormsModule } from '@angular/forms';
 import { FlashMessagesModule } from 'ngx-flash-messages';
 import { AlertModule } from 'ngx-bootstrap';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProfileComponent } from './components/profile/profile.component';
 
-// wrong import import { NavbarComponent } from './components/src/app/components/navbar/navbar.component';
 
 // Here we add the routes - later to rewrite with lasy loading
 
-const appRoutes : Routes = [
-  {path:'', component: HomeComponent},
-  {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent},
-  {path:'profile', component: ProfileComponent},
-  //{path: '**', component: NotFoundComponent},
-]
+// const appRoutes : Routes = [
+//   {path:'', component: HomeComponent},
+//   {path:'register', component: RegisterComponent},
+//   {path:'login', component: LoginComponent},
+//   {path:'profile', component: ProfileComponent},
+//   //{path: '**', component: NotFoundComponent},
+// ]
 
 @NgModule({
   declarations: [
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
     AppComponent,
     NavbarComponent,
-    LoginComponent,
-    RegisterComponent,
     HomeComponent,
-    ProfileComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    // RouterModule.forRoot(appRoutes),
     AlertModule.forRoot(),
     FlashMessagesModule
   ],
