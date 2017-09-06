7aaae76c852084ceed5dd0c98f0a5408b595c1d9
Milena Shiskova <shishkova.milena@gmail.com>
2017-09-06 13:24:49 +0300

    Trying to fix passport

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,14 +1,15 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+// const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config/database');
 
 module.exports = function(passport) {
     let opts = {}; // options // before it was fromAuthHeader();
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+    opts.jwtFromRequest = ExtractJwt.fromHeader('Authorization');
     opts.secretOrKey = config.secret;
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        console.log('test');
+        console.log(jwt_payload);
         User.getUserById(jwt_payload._id, (err, user) => {
             if (err) {
                 return done(err, false);
