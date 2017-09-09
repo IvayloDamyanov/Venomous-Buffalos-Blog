daad0f3f61d69a40f8fbb1f9e94ecc98d5ff32cc
Ivaylo Damyanov <ivaylo.damyanov@gmail.com>
2017-09-09 19:21:05 +0300

    resolved posts.js conflict

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -23,35 +23,32 @@ router.post('/newpost', (req, res) => {
     });
     */
 
-<<<<<<< HEAD
     Post.addPost(newPost, (err, post) => {
-=======
-    if (!req.body.name) {
-        res.json({ success: false, message: 'Post title is required.' });
-    }
-    if (!req.body.text) {
-        res.json({ success: false, message: 'Post content is required.' });
-    }
-    if (!req.body.ownerName) {
-        res.json({ success: false, message: 'Post creator is required.' });
-    }
-
-    const post = new Post({
-        ownerName: req.body.ownerName,
-        name: req.body.name,
-        img: req.body.img,
-        text: req.body.text,
-        // dateCreated: dateCreated
-    });
-
-    post.save((err) => {
->>>>>>> 3b28e7a8facd737d5d84cafc30f1bfb98558ac6b
-        if (err) {
-            res.json({ success: false, msg: 'Failed to add post' });
+        if (!req.body.name) {
+            res.json({ success: false, message: 'Post title is required.' });
+        }
+        if (!req.body.text) {
+            res.json({ success: false, message: 'Post content is required.' });
+        }
+        if (!req.body.ownerName) {
+            res.json({ success: false, message: 'Post creator is required.' });
         }
 
-        res.json({ success: true, msg: 'Post added' });
-    });
+        const post = new Post({
+            ownerName: req.body.ownerName,
+            name: req.body.name,
+            img: req.body.img,
+            text: req.body.text,
+            // dateCreated: dateCreated
+        });
+
+        post.save((err) => {
+            if (err) {
+                res.json({ success: false, msg: 'Failed to add post' });
+            }
+
+            res.json({ success: true, msg: 'Post added' });
+        });
 
     // Post.addPost(newPost, (err, post) => {
     //     console.log("adding post");
@@ -61,6 +58,7 @@ router.post('/newpost', (req, res) => {
     //         res.json({ success: true, msg: 'Post added' });
     //     }
     // });
+    });
 });
 
 module.exports = router;
