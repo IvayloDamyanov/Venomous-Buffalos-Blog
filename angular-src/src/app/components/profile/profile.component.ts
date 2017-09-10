a09522948d46d7847d2d042576f716b63b34072b
Milena Shiskova <shishkova.milena@gmail.com>
2017-09-10 23:16:15 +0300

    Editted profile

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -10,22 +10,7 @@ import { AuthService } from './../../services/auth.service';
 export class ProfileComponent implements OnInit {
   user: Object;
 
-  // TODO rewrite with function
- posts = [{
-   user:'Todor',
-   title:'TElerik',
-   text:`Hi! I'm New York Times best selling author Nomadic Matt. If you're overwhelmed by all the travel information on the web, sign up here to get proven 
-   step by step tips and tricks that will save you time, money, and have you traveling sooner!`,
-  pic:'../../../assets/imageOne.jpeg'
- },
- {
-  user:'Anya',
-  title:'Pernik',
-  text:`Hi! I'm New York Times best selling author Nomadic Matt. If you're overwhelmed by all the travel information on the web, sign up here to get proven 
-  step by step tips and tricks that will save you time, money, and have you traveling sooner!`,
-  pic:"../../../assets/logo-blog.png"
-
-}]
+  
 
   constructor( private authService: AuthService, private router:Router ) { }
 
